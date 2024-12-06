2ac9d1b78995b43a7ad5a8781bb896b8d26347e3
jeffrey <jloaiza@consensussa.com>
2024-12-05 23:04:02 -0500

    Adicion de metodo para generar el equipo y software con IA

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -6,11 +6,15 @@ class gemini_service:
         genai.configure(api_key=os.environ["API_KEY"])
         self.model = genai.GenerativeModel("gemini-1.5-flash")
 
-    def generate_equipment_software(self, activities):
-        for activity in activities:
-            if activity["activity"] == "generate_equipment_software":
-                prompt = f"Eres un experto en la planeacion de proyectos, dime solamente un hardware (con el siguiente formato: procesador, ram, almacenamiento, graficos) y solamente un software (con el siguiente formato: nombre, una caracteristica) que se necesite para realizar la siguiente actividad: {activity}. NO JUSTIFIQUES NI AÑADAS NOTAS, SOLO DIME LO QUE PEDI"
-                
-                response = self.model.generate_content(prompt)
+    def generate_equipment_software(self, task, result):
 
-                print(response)
+        prompt = f"Eres un experto en la planeacion de proyectos, dime solamente un hardware (con el siguiente formato: equipo:, características: procesador, ram, almacenamiento), solamente un software (con el siguiente formato: nombre: , caracteristica:) y solamente una justificación (con el siguiente formato: justificación) que se necesite para realizar la siguiente actividad: {task} y obtener el siguiente resultado: {result}. NO JUSTIFIQUES NI AÑADAS NOTAS, NO ME CAMBIES LOS FORMATOS NI LE AGREGUES TITULOS O SUBTITULOS A LA RESPUESTA, SOLO DIME LO QUE PEDI"
+        
+        response = self.model.generate_content(prompt)
+        print(response.text)
+
+        return response.text
+
+        
+
+        
